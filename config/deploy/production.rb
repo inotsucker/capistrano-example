925edded4bb106b53685a5d6177fe5aa355b68ea
WJ <weijianl-wa14@student.tarc.edu.my>
2018-10-26 15:35:25 +0800

    CI

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
 set :deploy_to, "/opt/lampp/htdocs/capistrano_example"
 
-server '172.104.170.27', user: 'deploy', roles: %w{web app db}
+#server '172.104.170.27', user: 'deploy', roles: %w{web app db}
+
+set :password, ask('Server password:', nil)
+server '172.104.170.27', user: 'deploy', password: fetch(:password), roles: %w{web app db}
