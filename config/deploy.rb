317fe8f460880f63df44e86d8c3b95ec62457149
WJ <weijianl-wa14@student.tarc.edu.my>
2018-10-26 18:18:46 +0800

    CI

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,30 +2,26 @@
 # require 'capistrano/ext/multistage'
 lock "~> 3.11.0"
 
+set :application, "capistrano_example"
+set :branch, "master"
+set :repo_url, "git@github.com:inotsucker/capistrano-example.git"
+set :deploy_user, "deploy"
+set :deploy_to, "/home/#{fetch :deploy_user}/#{fetch :application}"
+
+
+set :linked_dirs, %w(my_shared_directory)
+
 set :ssh_options, { 
   forward_agent: true,  
   keys: "~/.ssh/id_rsa" 
 }
 
-set :application, 'capistrano_example'
-set :branch, "master"
-set :repo_url, 'git@github.com:inotsucker/capistrano-example.git'
-
-set :deploy_to, "/var/www/#{fetch(:application)}/"
-set :deploy_user, "deploy"
-
-set :linked_dirs, %w(my_shared_directory)
-
-namespace :deploy do
-
-
-    desc 'Print The Server Name'
-    task :print_server_name do
-      on roles(:app), in: :groups, limit:1 do
-        execute "hostname"
-      end
-    end
-
-end
-
-after "deploy:updated", "deploy:print_server_name"
+#namespace :deploy do
+#    desc 'Print The Server Name'
+#    task :print_server_name do
+#      on roles(:app), in: :groups, limit:1 do
+#        execute "hostname"
+#      end
+#    end
+#end
+#after "deploy:updated", "deploy:print_server_name"
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,14 +1,17 @@
-set :stages, ["production"]
-set :default_stage, "production"
+#set :stages, ["production"]
 
-role :app, %w{deploy@172.104.170.27}
-role :web, %w{deploy@172.104.170.27}
-role :db,  %w{deploy@172.104.170.27}
+#set :default_stage, "production"
+
+#role :app, %w{deploy@172.104.170.27}
+#role :web, %w{deploy@172.104.170.27}
+#role :db,  %w{deploy@172.104.170.27}
 
 #server '172.104.170.27', user: 'deploy', roles: %w{web app db}
 
 #set :password, ask('Server password:', nil)
 #server '172.104.170.27', user: 'deploy', password: fetch(:password), roles: %w{web app db}
 
-		#set :password, ask(:password, nil, echo: false)
-		#server '172.104.170.27', user: 'deploy', port: 22, password: fetch(:password), roles: %w{web app db}
+#set :password, ask(:password, nil, echo: false)
+#server '172.104.170.27', user: 'deploy', port: 22, password: fetch(:password), roles: %w{web app db}
+
+server "172.104.170.27", user: "deploy", roles: ["app", "db", "web"]
